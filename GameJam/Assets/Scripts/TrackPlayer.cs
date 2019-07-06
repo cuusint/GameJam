6c6988cb9093aabf91b229665bd5c3d94603f6fb
ryphy0420 <ryphy0420@gmail.com>
2019-07-06 18:26:30 +0800

    2

---
+++ GameJam/Assets/Scripts/TrackPlayer.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TrackPlayer : MonoBehaviour
+{
+
+    public GameObject player;
+
+    private Vector3 targetPosition;
+
+    private Vector3 currentVelocity = Vector3.zero;
+
+
+    private void Update()
+    {
+        targetPosition = new Vector3(player.transform.position.x, player.transform.position.y + 2f, -10);
+        transform.position = Vector3.SmoothDamp(transform.position, targetPosition, ref currentVelocity, 0.001f);
+    }
+
+}
