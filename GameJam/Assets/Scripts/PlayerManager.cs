20f421c06e7ebd5916d5e66c831cad5866d8710d
FountainGattuso <fountaingattuso@foxmail.com>
2019-07-06 20:48:49 +0800

    加入素材

---
+++ GameJam/Assets/Scripts/PlayerManager.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+
+public class PlayerManager : MonoBehaviour {
+
+    private Rigidbody2D rb;
+    private BoxCollider2D collider;
+
+    private void Awake() {
+        rb = GetComponent<Rigidbody2D>();
+        collider = GetComponent<BoxCollider2D>();
+    }
+
+    private void FixedUpdate() {
+        //collider.enabled = rb.velocity.y > 0 ? false : true;
+    }
+
+}
